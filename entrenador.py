30c3f09acef6c49f049b15a9aae85f8bb1e21c26
juanjpachon <juanjpachon@javerianacali.edu.co>
2024-09-29 14:17:13 -0500

    change

--- entrenador.py
+++ entrenador.py
@@ -4,6 +4,7 @@
 import pygame
 import time
 import sys
+import cv2
 
 
 
@@ -42,23 +43,9 @@ fuente_titulo = pygame.font.SysFont('Arial', 36)
 fuente_texto = pygame.font.SysFont('Arial', 24)
 fuente_texto2= pygame.font.SysFont("Arial", 15)
 
-
-# def upload_gif(ruta_gif):
-#     gif = Image.open(ruta_gif)
-#     frames_gif = []
-#     while True:
-#         try:
-#             frame = gif.copy().convert("RGBA")  # Convertir el frame a modo RGBA para Pygame
-#             frame_pygame = pygame.image.fromstring(frame.tobytes(), frame.size, frame.mode)
-#             frames_gif.append(frame_pygame)
-#             gif.seek(gif.tell() + 1)
-#         except EOFError:
-#             pass
-#     return frames_gif 
-
-
-# frames_gif = upload_gif("media/pecho.gif")
-# frame_actual = 0
+# Variables globales para el sistema de puntos
+ejercicios_realizados = 0
+total_ejercicios_rutina = 4  # Definir cuántos ejercicios debe completar para felicitar
 
 
 
@@ -79,21 +66,147 @@ def show_text(texto, fuente, color, x, y):
     superficie_texto = fuente.render(texto, True, color)
     window.blit(superficie_texto, (x, y))
 
-def draw_grid():
-    # lINEAS HORIZONTALES
-    for y in range (0, HEIGHT, SIZE_CELL):
-        pygame.draw.line(window, GRAY, (0, y), (WIDTH, y))
+# def draw_grid():
+#     # lINEAS HORIZONTALES
+#     for y in range (0, HEIGHT, SIZE_CELL):
+#         pygame.draw.line(window, GRAY, (0, y), (WIDTH, y))
 
-    # LINEAS VERTICALES
-    for x in range (0, WIDTH, SIZE_CELL):
-        pygame.draw.line(window, GRAY, (x, 0), (x, HEIGHT))
+#     # LINEAS VERTICALES
+#     for x in range (0, WIDTH, SIZE_CELL):
+#         pygame.draw.line(window, GRAY, (x, 0), (x, HEIGHT))
+
+
+def play_exercise_video(ruta):
+    # Dimensiones de la ventana de video
+    video_width, video_height = 640, 480
+    video_window = pygame.display.set_mode((video_width, video_height))
+    pygame.display.set_caption("Video - Rutina de Ejercicio")
+
+    # Cargar el video usando OpenCV
+    video = cv2.VideoCapture(ruta)
+    clock = pygame.time.Clock()
+
+    running_video = True
+    while running_video:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running_video = False
+
+        # Leer el frame actual del video
+        ret, frame = video.read()
+        if ret:
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_surface = pygame.surfarray.make_surface(frame_rgb)
+            frame_surface = pygame.transform.rotate(frame_surface, 270) # rotar video segun angulo
+            frame_surface = pygame.transform.scale(frame_surface, (video_width, video_height))
+            video_window.blit(frame_surface, (0, 0))
+            pygame.display.update()
+            clock.tick(30)  # Controlar la tasa de frames por segundo del video
+        else:
+            running_video = False  # Cuando el video termina, cerramos la ventana
+
+    video.release()
+
+    # Al finalizar el video, restauramos la ventana principal
+    pygame.display.set_mode((WIDTH, HEIGHT))  # Restaurar el tamaño de la ventana principal
+    pygame.display.set_caption('Entrenador Personal BodyBuilder')
+
+# Función para simular un ejercicio
+def iniciar_ejercicio(ejercicio, series, repeticiones, descanso=5, video_path=None):
+    global ejercicios_realizados
+
+    # Si se proporciona una ruta de video, se reproduce el video del ejercicio
+    if video_path:
+        play_exercise_video(video_path)
+
+    # Después de reproducir el video, procedemos con el flujo del ejercicio
+    window.fill(WHITE)
+    show_text(f'Iniciando {ejercicio}', fuente_titulo, BLACK, 100, 50)
+    pygame.display.update()
+
+    serie_actual = 1
+    descanso_iniciado = False
+    tiempo_inicio_descanso = 0
+
+    clock = pygame.time.Clock()
+    repeticion_actual = 0
+
+    running_ejercicio = True
+    while running_ejercicio:
+        window.fill(WHITE)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running_ejercicio = False
+
+        # Mostrar información del ejercicio actual
+        if repeticion_actual < repeticiones:
+            show_text(f'Serie {serie_actual} - Repetición {repeticion_actual + 1}', fuente_texto, BLACK, 100, 150) # Numero de serie
+            show_text(f'Rep. restantes: {repeticiones - repeticion_actual - 1}', fuente_texto, BLACK, 100, 200) # Numero de repeticiones restantes
+            repeticion_actual += 1
+            pygame.display.update()
+            clock.tick(1)  # Simular una repetición por segundo
+
+        # Si se completaron las repeticiones, iniciar descanso
+        elif serie_actual <= series and not descanso_iniciado:
+            descanso_iniciado = True
+            tiempo_inicio_descanso = pygame.time.get_ticks()
+
+        # Manejar el temporizador de descanso
+        elif descanso_iniciado:
+            tiempo_transcurrido = (pygame.time.get_ticks() - tiempo_inicio_descanso) // 1000
+            if tiempo_transcurrido < descanso:
+                show_text(f'Descanso entre series: {descanso - tiempo_transcurrido} segundos', fuente_texto, BLACK, 100, 150)
+                pygame.display.update()
+            else:
+                descanso_iniciado = False
+                repeticion_actual = 0
+                serie_actual += 1
+
+        # Si se completaron todas las series, terminar el ejercicio
+        if serie_actual > series:
+            show_text('Ejercicio Completado', fuente_texto, BLACK, 100, 300)
+            draw_button(250, 350, 150, 50, BLACK, GRAY, 'Regresar', WHITE)
+            pygame.display.update()
+            running_ejercicio = False
+
+            # Incrementar el contador de ejercicios realizados
+            ejercicios_realizados += 1
+
+            # Verificar si ha completado todos los ejercicios de la rutina
+            verificar_completacion_rutina()
+
+        clock.tick(30)  # Controlar la tasa de fotogramas por segundo (FPS)
+
+    time.sleep(2)
+    pygame.display.update()
+
+
+def verificar_completacion_rutina():
+    global ejercicios_realizados, total_ejercicios_rutina
+
+    if ejercicios_realizados >= total_ejercicios_rutina:
+        window.fill(WHITE)
+        show_text('¡Felicitaciones!', fuente_titulo, BLACK, 100, 200)
+        show_text('Has completado tu rutina.', fuente_titulo, BLACK, 100, 250)
+        pygame.display.update()
+        time.sleep(3)  # Pausa para que pueda ver el mensaje
+
+        # Reiniciar el contador de ejercicios
+        ejercicios_realizados = 0
+
+        # Retornar al menú principal
+        return_to_menu()
+
+def return_to_menu():
+    global view
+    view = 'menu'
+
+
 # Funcion para mostrar menu
-
 def show_menu():  # Vista menu
     window.fill(BLUE)
     show_text("Hola, soy tu entrenador personalizado", fuente_titulo, BLACK, 50, 50)
     show_text("selecciona la parte del cuerpo que deseas entrenar el dia de hoy:", fuente_texto, BLACK, 50, 100)
-    draw_grid() # Llamar a la funcion para dibujar la cuadricula
     draw_button(100, 200, 150, 50, ORANGE, GRAY, 'Tren Superior', BLACK)
     draw_button(100, 450, 150, 50, ORANGE, GRAY, 'Tren Inferior', BLACK)
     draw_button(600, 500, 150, 50, BLACK, GRAY, 'Salir', WHITE)
@@ -107,9 +220,9 @@ def show_pushUp():
     show_text("Acuéstate boca abajo, manos a la altura de los hombros y cuerpo recto,", fuente_texto2, BLACK, 50, 100)
     show_text("Baja Flexionando los codos y baja el pecho hacia el suelo.", fuente_texto2, BLACK, 50, 125)
     show_text("Sube, Empujando con las manos para volver a la posición inicial.", fuente_texto2, BLACK, 50, 150)
-    draw_grid()
     draw_button(450, 500, 150, 50, ORANGE, GRAY, 'siguiente', BLACK)
     draw_button(200, 500, 150, 50, BLACK, GRAY,  'Regresar', WHITE)
+    draw_button(300,250, 150, 100,GRAY, WHITE, 'Iniciar', BLACK)
     pygame.display.update()
 
 def show_pressBanca():
@@ -118,9 +231,10 @@ def show_pressBanca():
     show_text("Acuéstate boca abajo, manos a la altura de los hombros y cuerpo recto,", fuente_texto2, BLACK, 50, 100)
     show_text("Baja Flexionando los codos y baja el pecho hacia el suelo.", fuente_texto2, BLACK, 50, 125)
     show_text("Sube, Empujando con las manos para volver a la posición inicial.", fuente_texto2, BLACK, 50, 150)
-    draw_grid()
     draw_button(450, 500, 150, 50, ORANGE, GRAY, 'siguiente', BLACK)
     draw_button(200, 500, 150, 50, BLACK, GRAY,  'Regresar', WHITE)
+    draw_button(300,250, 150, 100,GRAY, WHITE, 'Iniciar', BLACK)
+
     pygame.display.update()
     pygame.display.update()
 
@@ -130,9 +244,9 @@ def show_CurlBicep():
     show_text("Acuéstate boca abajo, manos a la altura de los hombros y cuerpo recto,", fuente_texto2, BLACK, 50, 100)
     show_text("Baja Flexionando los codos y baja el pecho hacia el suelo.", fuente_texto2, BLACK, 50, 125)
     show_text("Sube, Empujando con las manos para volver a la posición inicial.", fuente_texto2, BLACK, 50, 150)
-    draw_grid()
     draw_button(450, 500, 150, 50, ORANGE, GRAY, 'siguiente', BLACK)
     draw_button(200, 500, 150, 50, BLACK, GRAY,  'Regresar', WHITE)
+    draw_button(300,250, 150, 100,GRAY, WHITE, 'Iniciar', BLACK)
     pygame.display.update()
     pygame.display.update()
 
@@ -142,15 +256,15 @@ def show_plancha():
     show_text("Acuéstate boca abajo, manos a la altura de los hombros y cuerpo recto,", fuente_texto2, BLACK, 50, 100)
     show_text("Baja Flexionando los codos y baja el pecho hacia el suelo.", fuente_texto2, BLACK, 50, 125)
     show_text("Sube, Empujando con las manos para volver a la posición inicial.", fuente_texto2, BLACK, 50, 150)
-    draw_grid()
     draw_button(450, 500, 150, 50, ORANGE, GRAY, 'Finalizar', BLACK)
     draw_button(200, 500, 150, 50, BLACK, GRAY,  'Regresar', WHITE)
+    draw_button(300,250, 150, 100,GRAY, WHITE, 'Iniciar', BLACK)
     pygame.display.update()
 
 def show_congratulations():
     window.fill(BLUE)
-    draw_grid()
     show_text('Felicitaciones ', fuente_titulo, BLACK, 280, 50)
+    show_text("Has ganado 100 puntos", fuente_texto, RED, 300, 100)
     show_text("Llegaste al final de tu rutina, eres un ¡crack!", fuente_texto, BLACK,175, 150)
     draw_button(300, 450, 200, 100, PURPLE, GRAY,  'Finalizar', WHITE)
     pygame.display.update()
@@ -161,9 +275,9 @@ def show_extensionRodillas():
     show_text("Acuéstate boca abajo, manos a la altura de los hombros y cuerpo recto,", fuente_texto2, BLACK, 50, 100)
     show_text("Baja Flexionando los codos y baja el pecho hacia el suelo.", fuente_texto2, BLACK, 50, 125)
     show_text("Sube, Empujando con las manos para volver a la posición inicial.", fuente_texto2, BLACK, 50, 150)
-    draw_grid()
     draw_button(450, 500, 150, 50, ORANGE, GRAY, 'siguiente', BLACK)
     draw_button(200, 500, 150, 50, BLACK, GRAY,  'Regresar', WHITE)
+    draw_button(300,250, 150, 100,GRAY, WHITE, 'Iniciar', BLACK)
     pygame.display.update()
 
 def show_desplazamientos():
@@ -172,9 +286,9 @@ def show_desplazamientos():
     show_text("Acuéstate boca abajo, manos a la altura de los hombros y cuerpo recto,", fuente_texto2, BLACK, 50, 100)
     show_text("Baja Flexionando los codos y baja el pecho hacia el suelo.", fuente_texto2, BLACK, 50, 125)
     show_text("Sube, Empujando con las manos para volver a la posición inicial.", fuente_texto2, BLACK, 50, 150)
-    draw_grid()
     draw_button(450, 500, 150, 50, ORANGE, GRAY, 'siguiente', BLACK)
     draw_button(200, 500, 150, 50, BLACK, GRAY,  'Regresar', WHITE)
+    draw_button(300,250, 150, 100,GRAY, WHITE, 'Iniciar', BLACK)
     pygame.display.update()
 
 def show_sentadillas():
@@ -183,9 +297,9 @@ def show_sentadillas():
     show_text("Acuéstate boca abajo, manos a la altura de los hombros y cuerpo recto,", fuente_texto2, BLACK, 50, 100)
     show_text("Baja Flexionando los codos y baja el pecho hacia el suelo.", fuente_texto2, BLACK, 50, 125)
     show_text("Sube, Empujando con las manos para volver a la posición inicial.", fuente_texto2, BLACK, 50, 150)
-    draw_grid()
     draw_button(450, 500, 150, 50, ORANGE, GRAY, 'siguiente', BLACK)
     draw_button(200, 500, 150, 50, BLACK, GRAY,  'Regresar', WHITE)
+    draw_button(300,250, 150, 100,GRAY, WHITE, 'Iniciar', BLACK)
     pygame.display.update()
 
 def show_flexionDeRodilla():
@@ -194,31 +308,10 @@ def show_flexionDeRodilla():
     show_text("Acuéstate boca abajo, manos a la altura de los hombros y cuerpo recto,", fuente_texto2, BLACK, 50, 100)
     show_text("Baja Flexionando los codos y baja el pecho hacia el suelo.", fuente_texto2, BLACK, 50, 125)
     show_text("Sube, Empujando con las manos para volver a la posición inicial.", fuente_texto2, BLACK, 50, 150)
-    draw_grid()
     draw_button(450, 500, 150, 50, ORANGE, GRAY, 'Finalizar', BLACK)
     draw_button(200, 500, 150, 50, BLACK, GRAY,  'Regresar', WHITE)
+    draw_button(300,250, 150, 100,GRAY, WHITE, 'Iniciar', BLACK)
     pygame.display.update()
-
-
-
-# Mostrar ejercicio
-# def show_exercise(name, frames_gif): 
-#     global frame_actual
-
-#     window.fill(WHITE)
-
-#     text_exercise = fuente_titulo.render(name, True, BLACK)
-#     window.blit(text_exercise, (400,100))
-
-#     #Mostrar gif
-#     window.blit(frames_gif[frame_actual], (350,100))
-
-#     # Actualizar gif 
-#     frame_actual = (frame_actual + 1) % len(frames_gif)
-#     draw_button(400, 500, 150, 50, BLACK, GRAY, 'Finalizar', WHITE)
-#     pygame.display.update()
-
-
 
 
 view = 'menu'
@@ -251,9 +344,6 @@ while running:
     elif view == 'FlexionRodillas':
         show_flexionDeRodilla()
 
-    # elif view == 'exercise':
-    #     show_exercise('Press de banca', frames_gif)
-
     #Obtener posicion mouse
     mouse_pos = pygame.mouse.get_pos()
 
@@ -278,24 +368,32 @@ while running:
                   view = 'menu'
                elif 450 <= mouse_pos[0] <= 600 and 500 <= mouse_pos[1] <= 550: #Condicion para ir al siguiente ejercicio
                   view = "presbanca"
+               elif 300 <= mouse_pos[0] <= 450 and 250 <= mouse_pos[1] <= 350:
+                   iniciar_ejercicio('Push Up', 3, 10, 5, 'Push_up.mp4')
                
           elif view == 'presbanca':
              if 200 <= mouse_pos[0] <= 350 and 500 <= mouse_pos[1] <= 550: #condicion para el ejercio anterior
                   view = 'pushUp'
              elif 450 <= mouse_pos[0] <= 600 and 500 <= mouse_pos[1] <= 550: #condicion para el siguiente ejercicio
                   view = 'CurlBicep'
+             elif 300 <= mouse_pos[0] <= 450 and 250 <= mouse_pos[1] <= 350:
+                   iniciar_ejercicio('Press Banca', 3, 10, 5, 'Presbanca.mp4')
              
           elif view == 'CurlBicep':
             if 200 <= mouse_pos[0] <= 350 and 500 <= mouse_pos[1] <= 550:
                 view = 'presbanca'
             elif 450 <= mouse_pos[0] <= 600 and 500 <= mouse_pos[1] <= 550:
                 view = 'Plancha'
+            elif 300 <= mouse_pos[0] <= 450 and 250 <= mouse_pos[1] <= 350:
+                   iniciar_ejercicio('CurlBicep', 3, 10, 5, 'CurlBicep.mp4')
              
           elif view == 'Plancha':
               if 200 <= mouse_pos[0] <= 350 and 500 <= mouse_pos[1] <= 550: #condicion para ir al ejercicio anterior 
                   view = 'CurlBicep'
               elif 450 <= mouse_pos[0] <= 600 and 500 <= mouse_pos[1] <= 550: #condición para finalizar la rutina 
                   view = "Felicitaciones"
+              elif 300 <= mouse_pos[0] <= 450 and 250 <= mouse_pos[1] <= 350:
+                   iniciar_ejercicio('Plancha', 3, 10, 5, 'Plancha.mp4')
           
           elif view == 'Felicitaciones':
                 if 300 <= mouse_pos[0] <= 500 and 450 <= mouse_pos[1] <= 550:
@@ -305,55 +403,33 @@ while running:
                if 200 <= mouse_pos[0] <= 350 and 500 <= mouse_pos[1] <= 550: #Condicion para regresar al menu principal
                   view = 'menu'
                elif 450 <= mouse_pos[0] <= 600 and 500 <= mouse_pos[1] <= 550: #Condicion para ir al siguiente ejercicio
-                  view = "Desplazamientos"  
+                  view = "Desplazamientos"
+               elif 300 <= mouse_pos[0] <= 450 and 250 <= mouse_pos[1] <= 350:
+                   iniciar_ejercicio('Extension de Rodillas', 3, 10, 5, 'ExtensionRodillas.mp4')  
 
           elif view == 'Desplazamientos':
                if 200 <= mouse_pos[0] <= 350 and 500 <= mouse_pos[1] <= 550: #Condicion para regresar al menu principal
                   view = 'ExtensionRodillas'
                elif 450 <= mouse_pos[0] <= 600 and 500 <= mouse_pos[1] <= 550: #Condicion para ir al siguiente ejercicio
-                  view = 'Sentadillas'  
+                  view = 'Sentadillas'
+               elif 300 <= mouse_pos[0] <= 450 and 250 <= mouse_pos[1] <= 350:
+                   iniciar_ejercicio('Desplazamientos', 3, 10, 5, 'Desplazamientos.mp4')     
 
           elif view == 'Sentadillas':
                if 200 <= mouse_pos[0] <= 350 and 500 <= mouse_pos[1] <= 550: #Condicion para regresar al menu principal
                   view = 'Desplazamientos'
                elif 450 <= mouse_pos[0] <= 600 and 500 <= mouse_pos[1] <= 550: #Condicion para ir al siguiente ejercicio
                   view = 'FlexionRodillas'
+               elif 300 <= mouse_pos[0] <= 450 and 250 <= mouse_pos[1] <= 350:
+                   iniciar_ejercicio('Sentadillas', 3, 10, 5, 'Sentadillas.mp4')   
         
           elif view == 'FlexionRodillas':
                if 200 <= mouse_pos[0] <= 350 and 500 <= mouse_pos[1] <= 550: #Condicion para regresar al menu principal
                   view = 'Sentadillas'
                elif 450 <= mouse_pos[0] <= 600 and 500 <= mouse_pos[1] <= 550: #Condicion para ir al siguiente ejercicio
                   view = "Felicitaciones"
-
-# # Menú de partes del cuerpo
-# def mostrar_menu_partes_cuerpo():
-#     ventana.fill(BLANCO)
-#     mostrar_texto('Seleccione la parte del cuerpo a entrenar:', fuente_titulo, NEGRO, 100, 50)
-#     mostrar_texto('1. Pecho', fuente_texto, NEGRO, 100, 150)
-#     mostrar_texto('2. Piernas', fuente_texto, NEGRO, 100, 200)
-#     mostrar_texto('3. Espalda', fuente_texto, NEGRO, 100, 250)
-#     mostrar_texto('4. Hombros', fuente_texto, NEGRO, 100, 300)
-#     mostrar_texto('5. Brazos', fuente_texto, NEGRO, 100, 350)
-#     pygame.display.update()
-
-# # Función para simular un ejercicio
-# def iniciar_ejercicio(ejercicio, series, repeticiones):
-#     ventana.fill(BLANCO)
-#     mostrar_texto(f'Iniciando {ejercicio}', fuente_titulo, NEGRO, 100, 50)
-#     serie_actual = 1
-#     for serie in range(series):
-#         for repeticion in range(repeticiones):
-#             ventana.fill(BLANCO)
-#             mostrar_texto(f'Serie {serie_actual} - Repetición {repeticion + 1}', fuente_texto, NEGRO, 100, 150)
-#             mostrar_texto(f'Rep. restantes: {repeticiones - repeticion - 1}', fuente_texto, NEGRO, 100, 200)
-#             mostrar_texto(f'Tiempo: {time.strftime("%H:%M:%S", time.gmtime(time.time()))}', fuente_texto, NEGRO, 100, 250)
-#             pygame.display.update()
-#             time.sleep(1)  # Simular la pausa entre repeticiones
-#         serie_actual += 1
-#     mostrar_texto('Ejercicio Completado', fuente_texto, NEGRO, 100, 300)
-#     pygame.display.update()
-#     time.sleep(2)
-
+               elif 300 <= mouse_pos[0] <= 450 and 250 <= mouse_pos[1] <= 350:
+                   iniciar_ejercicio('Flexión Rodillas', 3, 10, 5, 'FlexionRodillas.mp4')   
 
 
 pygame.quit()
